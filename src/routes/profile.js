e361611ed76cbe0c396672bb76b42920361e983d
Tejashri1128 <tejudatir@gmail.com>
2025-02-19 16:12:15 +0530

    updatePassword

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const profileRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
@@ -25,6 +27,30 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     await user.save();
     res.json({
       message: `${user.lastName}, your profile updated successfully`,
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).send("ERROR : " + error.message);
+  }
+});
+
+profileRouter.patch("/profile/updatePassword", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    const { oldPassword, newPassword } = req.body;
+    const isOldPasswordValid = await bcrypt.compare(oldPassword, user.password);
+    if (!isOldPasswordValid) {
+      throw new Error("Invalid Old Password");
+    }
+    const isNewPasswordValid = validator.isStrongPassword(newPassword);
+    if (!isNewPasswordValid) {
+      throw new Error("New Password is not Strong password!!!");
+    }
+    const newHashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = newHashedPassword;
+    await user.save();
+    res.json({
+      message: `${user.lastName}, your password updated successfully`,
       data: user,
     });
   } catch (error) {
